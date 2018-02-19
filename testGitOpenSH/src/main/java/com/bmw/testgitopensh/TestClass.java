c88fe4ffd70c69202195e3eef15ae29e07f52dce
QXU1114 <QXU1114@LW09736441.africa.bmw.corp>
2018-02-19 12:23:09 +0200

    This is a first commit

---
+++ testGitOpenSH/src/main/java/com/bmw/testgitopensh/TestClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bmw.testgitopensh;
+
+/**
+ *
+ * @author QXU1114
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("This is a GIT Test");
+    }
+}
